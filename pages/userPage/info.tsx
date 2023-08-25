468e829d46d3b8c1c090e69995d3513f5ec6d040
tzujie <tzujie.ning@gmail.com>
2023-08-25 16:06:07 +0800

    info

--- pages/userPage/info.tsx
+++ pages/userPage/info.tsx
@@ -1,33 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
 import axios from 'axios';
 import Cookies from 'js-cookie';
-im
-export default function info() {
-    const [userData, setUserData] = useState<any[]>([]);
+import styles from './info.module.css';
+
+function Info() {
+    const [userData, setUserData] = useState<any | null>(null);  // 初始化為null，代表資料還沒有被載入
 
     const getUserEmail = () => {
         const userEmail = Cookies.get('userEmail');
-        console.log("User Email:", userEmail);  // 新增的 console.log
+        console.log("User Email:", userEmail);
         return userEmail ? userEmail : null;
     };
 
     useEffect(() => {
-        // 獲取Email
         const userEmail = getUserEmail();
-        console.log("Fetched User Email:", userEmail);  // 新增的 console.log
+        console.log("Fetched User Email:", userEmail);
 
-        // 用戶登入->API
         if (userEmail) {
             const apiUrl = `https://b084-163-13-201-95.ngrok-free.app/api/accounts/?email=${encodeURIComponent(userEmail)}`;
 
             axios
                 .get(apiUrl)
                 .then((response) => {
-                    // 處理API
-                    const userData = response.data;
-
-                    //放到state
+                    const userData = response.data[0];  // 從陣列中取得第一個物件
                     setUserData(userData);
                 })
                 .catch((error) => {
@@ -36,33 +31,34 @@ export default function info() {
         }
     }, []);
 
-
     return (
         <div className={styles.container}>
-            <div className={styles.infoBox}>
-                <div className={styles.field}>
-                    <span className={styles.label}>ID:</span>
-                    {accountData.id}
+            {userData && (
+                <div className={styles.infoBox}>
+                    <div className={styles.field}>
+                        <span className={styles.label}>ID:</span>
+                        {userData.id}
+                    </div>
+                    <div className={styles.field}>
+                        <span className={styles.label}>Name:</span>
+                        {userData.name}
+                    </div>
+                    <div className={styles.field}>
+                        <span className={styles.label}>Email:</span>
+                        {userData.email}
+                    </div>
+                    <div className={styles.field}>
+                        <span className={styles.label}>Phone:</span>
+                        {userData.phone}
+                    </div>
+                    <div className={styles.field}>
+                        <span className={styles.label}>Login Count:</span>
+                        {userData.login_count}
+                    </div>
                 </div>
-                <div className={styles.field}>
-                    <span className={styles.label}>Name:</span>
-                    {accountData.name}
-                </div>
-                <div className={styles.field}>
-                    <span className={styles.label}>Email:</span>
-                    {accountData.email}
-                </div>
-                <div className={styles.field}>
-                    <span className={styles.label}>Phone:</span>
-                    {accountData.phone}
-                </div>
-                <div className={styles.field}>
-                    <span className={styles.label}>Login Count:</span>
-                    {accountData.login_count}
-                </div>
-            </div>
+            )}
         </div>
     );
-};
+}
 
 export default Info;
